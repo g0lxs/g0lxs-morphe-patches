53d5b79e78724dd84857a0471c568b447b77787c
g0lxs <105403623+g0lxs@users.noreply.github.com>
2026-09-02 16:53:05 -0300

    docs: update repository URLs to g0lxs-morphe-patches

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -4,10 +4,10 @@ patches {
     about {
         name = "g0lxs's Morphe Patches"
         description = "Personal Morphe patches by g0lxs."
-        source = "git@github.com:g0lxs/hoomans-morphe-patches.git"
+        source = "git@github.com:g0lxs/g0lxs-morphe-patches.git"
         author = "g0lxs"
         contact = "na"
-        website = "https://github.com/g0lxs/hoomans-morphe-patches"
+        website = "https://github.com/g0lxs/g0lxs-morphe-patches"
         license = "GPLv3"
     }
 }
