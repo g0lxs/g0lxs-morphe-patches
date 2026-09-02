5da3b8ed8b18dcb0de9886c749e5a1f21b79885c
g0lxs <105403623+g0lxs@users.noreply.github.com>
2026-09-02 17:49:41 -0300

    feat: neutralize native anti-tamper signature check in RogUroo 6.6.2

--- patches/src/main/kotlin/g0lxs/morphe/patches/oldroll/shared/Constants.kt
+++ patches/src/main/kotlin/g0lxs/morphe/patches/oldroll/shared/Constants.kt
@@ -1,6 +1,7 @@
 package g0lxs.morphe.patches.oldroll.shared
 
 import app.morphe.patcher.patch.ApkFileType
+import app.morphe.patcher.patch.AppTarget
 import app.morphe.patcher.patch.Compatibility
 
 object Constants {
@@ -9,5 +10,6 @@ object Constants {
         packageName = "com.accordion.analogcam",
         apkFileType = ApkFileType.APK,
         appIconColor = 0xFF6B35,
+        targets = listOf(AppTarget("6.6.2")),
     )
 }
