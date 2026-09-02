54cb09a88834c9eff0456c6732a202d3cb1bea53
g0lxs <105403623+g0lxs@users.noreply.github.com>
2026-09-02 17:20:57 -0300

    feat: add RogUroo Enable Pro patch

---
+++ patches/src/main/kotlin/g0lxs/morphe/patches/oldroll/premium/EnableProPatch.kt
@@ -0,0 +1,31 @@
+package g0lxs.morphe.patches.oldroll.premium
+
+import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
+import app.morphe.patcher.patch.bytecodePatch
+import g0lxs.morphe.patches.oldroll.shared.Constants.COMPATIBILITY_OLDROLL
+
+private const val TRUE_RETURN = """
+    const/4 v0, 0x1
+    return v0
+"""
+
+private const val RETURN_CURRENT_TIME = """
+    invoke-static {}, Ljava/lang/System;->currentTimeMillis()J
+    move-result-wide v0
+    return-wide v0
+"""
+
+@Suppress("unused")
+val enableOldrollProPatch = bytecodePatch(
+    name = "Enable Pro",
+    description = "Unlocks all pro/premium features by bypassing purchase validation and SharedPreferences checks.",
+    default = true,
+) {
+    compatibleWith(COMPATIBILITY_OLDROLL)
+
+    execute {
+        PurchaseSharedPrefManagerIsSkuPurchasedFingerprint.method.addInstructions(0, TRUE_RETURN)
+        PurchaseSharedPrefManagerGetProMonthPurchaseTimeFingerprint.method.addInstructions(0, RETURN_CURRENT_TIME)
+        PurchaseSharedPrefManagerGetProYearPurchaseTimeFingerprint.method.addInstructions(0, RETURN_CURRENT_TIME)
+    }
+}
